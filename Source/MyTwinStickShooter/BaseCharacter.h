cad187d0cd3c052afe3958311fbec6c97cf3ee13
bonomjy07 <49272857+bonomjy07@users.noreply.github.com>
2019-05-13 21:56:19 +0900

    Add files via upload

---
+++ Source/MyTwinStickShooter/BaseCharacter.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseCharacter.generated.h"
+
+UCLASS(Blueprintable)
+class MYTWINSTICKSHOOTER_API ABaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	// Make a health property
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "BaseCharacter")
+		float Health = 100;
+
+	// Make a isDead property
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "BaseCharacter")
+		bool isDead = false;
+
+
+	// Calculate death function (helper)
+	virtual void CalculateDead();
+
+	// Calculate heath function
+	UFUNCTION(BlueprintCallable, Category = "BaseCharacter")
+		virtual void CalculateHealth(float delta);
+
+
+	// Editor-centric code for changeing properies
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+		override;
+#endif
+
+
+
+public:
+	// Sets default values for this character's properties
+	ABaseCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
